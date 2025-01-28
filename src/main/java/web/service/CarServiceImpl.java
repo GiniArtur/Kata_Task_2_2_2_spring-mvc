791a059c08cac3808f10db175a56bfcf67e5451a
Artur Giniyatullin <archigini@gmail.com>
2025-01-28 19:45:48 +0500

    changed metod getCarsList

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
-    private final List<Car> carsList = new ArrayList<>(List.of(
+    private final List<Car> cars = new ArrayList<>(List.of(
             new Car(1L, 2015, "Mazda", "jvbsjbvjs1"),
             new Car(2L, 2014, "Mazda", "jvbsjbvjs2"),
             new Car(3L, 2020, "Mazda", "jvbsjbvjs3"),
@@ -17,14 +17,13 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public List<Car> getCarsList(Integer count) {
-        if (count == null || count > 5) {
-            count = 5;
+        List<Car> carsList = new ArrayList<>();
+        if (count == null || count <= 0) {
+            count = cars.size();
         }
-        if (count < 1) {
-            count = 0;
-        } else {
-            count = count;
+        for (int i = 0; i < count && i < cars.size(); i++) {   //-1
+            carsList.add(cars.get(i));
         }
-        return carsList.stream().limit(count).toList();
+        return carsList;
     }
 }
